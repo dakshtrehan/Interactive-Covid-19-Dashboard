35106f65efa6a7717faa4c452e24667c1880224d
priyansh <singhalpriyansh58@gmail.com>
2020-12-27 19:57:32 +0530

    Middleware Updated

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
